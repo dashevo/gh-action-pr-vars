99f99a4a9cc83736a199c229eb759182fcba91c4
strophy <badfunkstripe@gmail.com>
2021-03-30 17:31:22 +0200

    fix: can't load pr body from input

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 
-const pullRequestBody = core.getInput('pull-request-body');
+const pullRequestBody = github.context.payload.pull_request.body
 
 pullRequestBody
   .split('\n')
   .filter((line) => line.startsWith("/set-var"))
   .map((line) => line.replace("/set-var ", ''))
   .forEach((line) => {
+    line.replace(/\r$/, '');
     const [key, value] = line.split('=');
-
     core.setOutput(key, value);
   });
-
