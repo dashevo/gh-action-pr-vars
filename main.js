c4b3728f33a04c6aee084160de9e8d7638419f44
strophy <badfunkstripe@gmail.com>
2021-04-18 13:52:45 +0200

    remove debug

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-console.log(github.context);
 const pullRequestBody = github.context.payload.pull_request.body
 
 pullRequestBody
