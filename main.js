34d1d7612f3c2ed104154516631c0b472e8d7bb1
strophy <badfunkstripe@gmail.com>
2021-04-18 13:33:54 +0200

    output debug

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
+console.log(github);
 const pullRequestBody = github.context.payload.pull_request.body
 
 pullRequestBody
